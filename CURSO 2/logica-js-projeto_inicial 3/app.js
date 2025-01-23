631190905f486f3fe772b8a320ac9cfbac31ab0e
lililap <lidialapertosa@gmail.com>
2025-01-23 10:49:36 -0300

    Delete CURSO 2 directory

--- CURSO 2/logica-js-projeto_inicial 3/app.js
+++
@@ -1,6 +0,0 @@
-let listaGenerica = [];
-let linguagensDeProgramacao = ['Javascript','C','C++','Kotlin','Pyhton'];
-
-linguagensDeProgramacao.push('Java','Ruby','GoLang');
-
-console.log(linguagensDeProgramacao[0]);
